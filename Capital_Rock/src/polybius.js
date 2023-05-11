32a6132ca6642db4bc57b6e281cd80ce175abbda
Thomas Styron <thomasstyron@gmail.com>
2023-05-11 17:40:10 -0500

    12 is almost done

--- Capital_Rock/src/polybius.js
+++ Capital_Rock/src/polybius.js
@@ -4,12 +4,103 @@
 // of the anonymous function on line 6
 
 const polybiusModule = (function () {
-  // you can add any code you want within this function scope
+  const breakCode = {
+    a: "11",
+    b: "21",
+    c: "31",
+    d: "41",
+    e: "51",
+    f: "12",
+    g: "22",
+    h: "32",
+    i: "42",
+    j: "42",
+    k: "52",
+    l: "13",
+    m: "23",
+    n: "33",
+    o: "43",
+    p: "53",
+    q: "14",
+    r: "24",
+    s: "34",
+    t: "44",
+    u: "54",
+    v: "15",
+    w: "25",
+    x: "35",
+    y: "45",
+    z: "55",
+  };
 
-  function polybius(input, encode = true) {
-    // your solution code here
+  const makeCode = {
+    11: "a",
+    12: "f",
+    13: "l",
+    14: "q",
+    15: "v",
+    21: "b",
+    22: "g",
+    23: "m",
+    24: "r",
+    25: "w",
+    31: "c",
+    32: "h",
+    33: "n",
+    34: "s",
+    35: "x",
+    41: "d",
+    42: "i/j",
+    43: "o",
+    44: "t",
+    45: "y",
+    51: "e",
+    52: "k",
+    53: "p",
+    54: "u",
+    55: "z",
+  };
+
+ 
+  
+
+  function decode(input) {
+  if(input.replace(" ","").length %2 !== 0){
+    return false
   }
-
+    message = "";
+    for (let i = 0; i < input.length; i += 2) {
+      console.log("input[i] is equal to ",input[i])
+      if (input[i] === " ") {
+        i++;
+        message += " "
+      }
+      message += makeCode[`${input[i]}${input[i + 1]}`];
+    }
+    return message
+   }
+  
+  // your solution code here
+  // }
+  
+function polybius(input, encode = true) {
+  if(encode === false){ 
+    return decode(input)
+    }
+    let code = "";
+    let inputs = input.toLowerCase(); //get everything the same
+    for (let i = 0; i < inputs.length; i++) {
+      // regular for loop?
+      letter = inputs[i]; // spaces, again  symbols?
+      if (letter === " ") {
+        code += " ";
+        continue; // i love continue
+      }
+      const letterToNumber = breakCode[letter]; //matchup input letter to obj[letter]
+      code += letterToNumber;
+    }
+    return code;
+  }
   return {
     polybius,
   };
--- Capital_Rock/src/substitution.js
+++ Capital_Rock/src/substitution.js
@@ -4,10 +4,70 @@
 // of the anonymous function on line 6
 
 const substitutionModule = (function () {
+  function decode(input,alphabet) {
+  //   // if(!alphabet || alphabet.length !== 26
+  //   //    || alphabet.length !== new Set (alphabet) .size){
+  //   //   return false
+  //   // }
+     
+    let message = ""
+  //   // loop through input
+     for(let i=0;i<input.length;i++){
+         let letter = input[i]
+  //       //again, spaces
+        if(letter === " "){
+            message += " "
+             continue   //<-- still my new best friend
+         }
+  //       //symbols !@#$%^&*()? etc
+          let LetterIdx = alphabet
+         .indexOf(letter.toLowerCase())
+         if(LetterIdx ===-1){
+             message += letter
+             continue
+        }
+  //       // build the wonky input version of reg input
+       let newLetter ='abcdefghijklmnopqrstuvwxyz'[LetterIdx]
+         message += newLetter
+      }
+       return message  
+    }
+    
+  //   return message
+    
+  // }
   // you can add any code you want within this function scope
 
   function substitution(input, alphabet, encode = true) {
-    // your solution code here
+    if(!alphabet || alphabet.length !== 26
+       || alphabet.length !== new Set (alphabet) .size){
+      return false 
+    }
+     if(encode === false){
+      return decode(input, alphabet)
+     }   //return decode()
+    let wonky = ""
+    // loop through input
+    for(let i=0;i<input.length;i++){
+        let letter = input[i]
+        //again, spaces
+        if(letter === " "){
+            wonky += " "
+            continue   //<-- still my new best friend
+        }
+        //symbols !@#$%^&*()? etc
+        let regLetterIdx = 'abcdefghijklmnopqrstuvwxyz'
+        .indexOf(letter.toLowerCase())
+        if(regLetterIdx ===-1){
+            wonky += letter
+            continue
+        }
+        // build the wonky input version of reg input
+       let wonkyLetter = alphabet[regLetterIdx]
+        wonky += wonkyLetter
+    }
+    return wonky
+    
   }
 
   return {
